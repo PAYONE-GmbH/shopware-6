016365bd295dee8d7f3e51f87db5efbe8dd87492
Uwe Kleinmann <uwe@kleinmann.org>
2019-05-08 17:44:16 +0200

    [PAYONE-5] Add credit card preauthorization payment.

---
+++ src/Payone/Request/CreditCard/CreditCardPreAuthorizeRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayonePayment\Payone\Request\CreditCard;
+
+use PayonePayment\Payone\Struct\PaymentTransactionStruct;
+use Shopware\Core\Checkout\Payment\Exception\InvalidOrderException;
+use Shopware\Core\Framework\Context;
+
+class CreditCardPreAuthorizeRequest
+{
+    public function getRequestParameters(PaymentTransactionStruct $transaction, string $pseudoPan, Context $context): array
+    {
+        if (empty($transaction->getReturnUrl())) {
+            throw new InvalidOrderException($transaction->getOrder()->getId());
+        }
+
+        $order = $transaction->getOrder();
+
+        if (null === $order) {
+            throw new InvalidOrderException($transaction->getOrder()->getId());
+        }
+
+        return [
+            'request'       => 'preauthorization',
+            'clearingtype'  => 'cc',
+            'amount'        => (int) ($order->getAmountTotal() * 100),
+            'currency'      => $order->getCurrency()->getShortName(),
+            'reference'     => $order->getOrderNumber(),
+            'pseudocardpan' => $pseudoPan,
+            'successurl'    => $transaction->getReturnUrl() . '&state=success',
+            'errorurl'      => $transaction->getReturnUrl() . '&state=error',
+            'backurl'       => $transaction->getReturnUrl() . '&state=cancel',
+        ];
+    }
+}
