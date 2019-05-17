e2a379042ef0285ef928c24619b071af656fd62a
Jochen Manz <j.manz@kellerkinder.de>
2019-05-17 10:04:14 +0200

    [PAYONE-26] basic debit card implementation

--- src/PaymentHandler/PayoneDebitPaymentHandler.php
+++ src/PaymentHandler/PayoneDebitPaymentHandler.php
@@ -4,14 +4,116 @@ declare(strict_types=1);
 
 namespace PayonePayment\PaymentHandler;
 
+use DateTime;
+use PayonePayment\Installer\CustomFieldInstaller;
+use PayonePayment\Payone\Client\Exception\PayoneRequestException;
+use PayonePayment\Payone\Client\PayoneClientInterface;
+use PayonePayment\Payone\Request\Debit\DebitAuthorizeRequestFactory;
+use PayonePayment\Payone\Struct\PaymentTransactionStruct;
+use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionStates;
 use Shopware\Core\Checkout\Payment\Cart\PaymentHandler\SynchronousPaymentHandlerInterface;
 use Shopware\Core\Checkout\Payment\Cart\SyncPaymentTransactionStruct;
+use Shopware\Core\Checkout\Payment\Exception\SyncPaymentProcessException;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Shopware\Core\System\StateMachine\StateMachineRegistry;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Throwable;
 
 class PayoneDebitPaymentHandler implements SynchronousPaymentHandlerInterface
 {
+    /** @var DebitAuthorizeRequestFactory */
+    private $requestFactory;
+
+    /** @var PayoneClientInterface */
+    private $client;
+
+    /** @var EntityRepositoryInterface */
+    private $transactionRepository;
+
+    /** @var StateMachineRegistry */
+    private $stateMachineRegistry;
+
+    /** @var TranslatorInterface */
+    private $translator;
+
+    /** @var RequestStack */
+    private $requestStack;
+
+    public function __construct(
+        DebitAuthorizeRequestFactory $requestFactory,
+        PayoneClientInterface $client,
+        EntityRepositoryInterface $transactionRepository,
+        StateMachineRegistry $stateMachineRegistry,
+        TranslatorInterface $translator,
+        RequestStack $requestStack
+    ) {
+        $this->requestFactory        = $requestFactory;
+        $this->client                = $client;
+        $this->transactionRepository = $transactionRepository;
+        $this->stateMachineRegistry  = $stateMachineRegistry;
+        $this->translator            = $translator;
+        $this->requestStack          = $requestStack;
+    }
+
     public function pay(SyncPaymentTransactionStruct $transaction, SalesChannelContext $salesChannelContext): void
     {
-        // TODO: call payone and handle debit card data, maybe save for later
+        $paymentTransaction = PaymentTransactionStruct::fromSyncPaymentTransactionStruct($transaction);
+
+        $request = $this->requestFactory->getRequestParameters(
+            $paymentTransaction,
+            $salesChannelContext->getContext()
+        );
+
+        try {
+            // TODO: WHAT! request leads to a "Configuration problem, please choose another payment method." exception
+            $response = $this->client->request($request);
+        } catch (PayoneRequestException $exception) {
+            throw new SyncPaymentProcessException(
+                $transaction->getOrderTransaction()->getId(),
+                $exception->getResponse()['error']['CustomerMessage']
+            );
+        } catch (Throwable $exception) {
+            throw new SyncPaymentProcessException(
+                $transaction->getOrderTransaction()->getId(),
+                $this->translator->trans('PayonePayment.errorMessages.genericError')
+            );
+        }
+
+        $key = (new DateTime())->format(DATE_ATOM);
+
+        $customFields = $transaction->getOrderTransaction()->getCustomFields() ?? [];
+
+        $customFields[CustomFieldInstaller::TRANSACTION_ID]         = (string) $response['txid'];
+        $customFields[CustomFieldInstaller::SEQUENCE_NUMBER]        = 1;
+        $customFields[CustomFieldInstaller::TRANSACTION_DATA][$key] = $response;
+
+        $data = [
+            'id'           => $transaction->getOrderTransaction()->getId(),
+            'customFields' => $customFields,
+        ];
+
+        $this->transactionRepository->update([$data], $salesChannelContext->getContext());
+
+        if (strtolower($response['status']) === 'error') {
+            throw new SyncPaymentProcessException(
+                $transaction->getOrderTransaction()->getId(),
+                $this->translator->trans('PayonePayment.errorMessages.genericError')
+            );
+        }
+
+        $completeState = $this->stateMachineRegistry->getStateByTechnicalName(
+            OrderTransactionStates::STATE_MACHINE,
+            OrderTransactionStates::STATE_PAID,
+            $salesChannelContext->getContext()
+        );
+
+        $data = [
+            'id'      => $transaction->getOrderTransaction()->getId(),
+            'stateId' => $completeState->getId(),
+        ];
+
+        $this->transactionRepository->update([$data], $salesChannelContext->getContext());
     }
 }
