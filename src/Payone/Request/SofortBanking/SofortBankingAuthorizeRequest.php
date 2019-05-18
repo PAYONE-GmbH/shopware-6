51b72521f19e314dc3f460495533c841e9fbb10d
Jochen Manz <j.manz@kellerkinder.de>
2019-05-18 13:49:08 +0200

    [PAYONE-35] document reference amount changes

--- src/Payone/Request/SofortBanking/SofortBankingAuthorizeRequest.php
+++ src/Payone/Request/SofortBanking/SofortBankingAuthorizeRequest.php
@@ -19,6 +19,10 @@ class SofortBankingAuthorizeRequest
         $this->redirectHandler = $redirectHandler;
     }
 
+    /**
+     * TODO: the reference number needs to be unique. When multiple transactions are possible per order, we need to add
+     * TODO: a suffix/prefix or use another number as reference
+     */
     public function getRequestParameters(
         PaymentTransactionStruct $transaction,
         Context $context
