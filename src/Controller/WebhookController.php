c0734c2c04f14d02b30844cd8a412683abce72c9
Jochen Manz <j.manz@kellerkinder.de>
2019-06-17 14:57:00 +0200

    [PAYONE-39] fix phpstan errors

--- src/Controller/WebhookController.php
+++ src/Controller/WebhookController.php
@@ -6,7 +6,7 @@ namespace PayonePayment\Controller;
 
 use PayonePayment\Payone\Webhook\Processor\WebhookProcessorInterface;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
-use Shopware\Storefront\Framework\Controller\StorefrontController;
+use Shopware\Storefront\Controller\StorefrontController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
