d2f89b72b9c62cd619c5f92673b3b9428f4ccc88
Jochen Manz <j.manz@kellerkinder.de>
2019-06-24 15:00:58 +0200

    [PAYONE-41] save credit card data after usage to customer

---
+++ src/DataAbstractionLayer/Entity/Card/PayonePaymentCardEntity.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayonePayment\DataAbstractionLayer\Entity\Card;
+
+use Shopware\Core\Checkout\Customer\CustomerEntity;
+use Shopware\Core\Framework\DataAbstractionLayer\Entity;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityIdTrait;
+
+class PayonePaymentCardEntity extends Entity
+{
+    use EntityIdTrait;
+
+    /** @var string */
+    private $pseudoCardPan;
+
+    /** @var string */
+    private $truncatedCardPan;
+
+    /** @var null|CustomerEntity */
+    private $customer;
+
+    /** @var null|string */
+    private $customerId;
+
+    public function getPseudoCardPan(): string
+    {
+        return $this->pseudoCardPan;
+    }
+
+    public function setPseudoCardPan(string $pseudoCardPan): void
+    {
+        $this->pseudoCardPan = $pseudoCardPan;
+    }
+
+    public function getTruncatedCardPan(): string
+    {
+        return $this->truncatedCardPan;
+    }
+
+    public function setTruncatedCardPan(string $truncatedCardPan): void
+    {
+        $this->truncatedCardPan = $truncatedCardPan;
+    }
+
+    public function getCustomer(): ?CustomerEntity
+    {
+        return $this->customer;
+    }
+
+    public function setCustomer(CustomerEntity $customer): void
+    {
+        $this->customer = $customer;
+    }
+
+    public function getCustomerId(): ?string
+    {
+        return $this->customerId;
+    }
+}
---
+++ src/Migration/Migration1561379069AddPayonePaymentCardTable.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayonePayment\Migration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+class Migration1561379069AddPayonePaymentCardTable extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1561379069;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $connection->exec('
+            
+            CREATE TABLE `payone_payment_card` (
+                `id` BINARY(16) NOT NULL,
+                
+                `customer_id` BINARY(16) NOT NULL,
+                
+                `pseudo_card_pan` VARCHAR(255) NOT NULL,
+                `truncated_card_pan` VARCHAR(255) NOT NULL,
+                
+                `created_at` DATETIME(3) NOT NULL,
+                `updated_at` DATETIME(3) NULL,
+                
+                PRIMARY KEY (`id`),
+                
+                KEY `fk.payone_payment_card.customeri_id` (`customer_id`),
+                
+                CONSTRAINT `fk.payone_payment_card.customeri_id` 
+                    FOREIGN KEY (`customer_id`) 
+                    REFERENCES `customer` (`id`) 
+                    ON DELETE RESTRICT ON UPDATE CASCADE
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+        ');
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+        // implement update destructive
+    }
+}
