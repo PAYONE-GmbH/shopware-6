83755f0bcd1c6384500be09a2e2aac9f4fdf256d
Jochen Manz <j.manz@kellerkinder.de>
2019-06-27 13:38:15 +0200

    [PAYONE-39] remove ordernumber prefix

--- src/Payone/Request/SofortBanking/SofortBankingAuthorizeRequest.php
+++ src/Payone/Request/SofortBanking/SofortBankingAuthorizeRequest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace PayonePayment\Payone\Request\SofortBanking;
 
-use PayonePayment\Components\ConfigReader\ConfigReaderInterface;
 use PayonePayment\Components\RedirectHandler\RedirectHandler;
 use PayonePayment\Payone\Struct\PaymentTransaction;
 use RuntimeException;
@@ -23,30 +22,18 @@ class SofortBankingAuthorizeRequest
     /** @var EntityRepositoryInterface */
     private $currencyRepository;
 
-    /** @var ConfigReaderInterface */
-    private $configReader;
-
     public function __construct(
         RedirectHandler $redirectHandler,
-        EntityRepositoryInterface $currencyRepository,
-        ConfigReaderInterface $configReader
+        EntityRepositoryInterface $currencyRepository
     ) {
         $this->redirectHandler    = $redirectHandler;
         $this->currencyRepository = $currencyRepository;
-        $this->configReader       = $configReader;
     }
 
     public function getRequestParameters(PaymentTransaction $transaction, Context $context): array
     {
         if (empty($transaction->getReturnUrl())) {
             throw new InvalidOrderException($transaction->getOrder()->getId());
-        }
-
-        $config    = $this->configReader->read($transaction->getOrder()->getSalesChannelId());
-        $reference = $transaction->getOrder()->getOrderNumber();
-
-        if (!empty($config->get('ordernumberPrefix'))) {
-            $reference = $config->get('ordernumberPrefix') . $reference;
         }
 
         return [
@@ -56,7 +43,7 @@ class SofortBankingAuthorizeRequest
             'bankcountry'            => 'DE', // TODO: possible values DE, AT, CH, NL
             'amount'                 => (int) ($transaction->getOrder()->getAmountTotal() * 100),
             'currency'               => $this->getOrderCurrency($transaction->getOrder(), $context)->getIsoCode(),
-            'reference'              => $reference,
+            'reference'              => $transaction->getOrder()->getOrderNumber(),
             'successurl'             => $this->redirectHandler->encode($transaction->getReturnUrl() . '&state=success'),
             'errorurl'               => $this->redirectHandler->encode($transaction->getReturnUrl() . '&state=error'),
             'backurl'                => $this->redirectHandler->encode($transaction->getReturnUrl() . '&state=cancel'),
