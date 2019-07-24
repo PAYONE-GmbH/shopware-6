a9d9931e50d6d8a9874901725e60061102ba1f29
Jochen Manz <j.manz@kellerkinder.de>
2019-07-24 18:33:00 +0200

    [PAYONE-28] clear cache on payment method changes and enhance the cache key with the saleschannel id

--- src/EventListener/StorefrontRenderEventListener.php
+++ src/EventListener/StorefrontRenderEventListener.php
@@ -1,11 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace PayonePayment\EventListener;
 
 use PayonePayment\Installer\CustomFieldInstaller;
+use PayonePayment\Installer\PaymentMethodInstaller;
 use Psr\Cache\CacheItemPoolInterface;
+use Shopware\Core\Checkout\Payment\PaymentMethodDefinition;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityWrittenContainerEvent;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\System\SalesChannel\Aggregate\SalesChannelPaymentMethod\SalesChannelPaymentMethodDefinition;
 use Shopware\Core\System\SalesChannel\Entity\SalesChannelRepositoryInterface;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Event\StorefrontRenderEvent;
@@ -13,38 +21,101 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class StorefrontRenderEventListener implements EventSubscriberInterface
 {
-    private const CACHE_ID = 'payone_payment.menu_state';
-
     /** @var CacheItemPoolInterface */
-    private $cache;
+    private $cachePool;
 
     /** @var SalesChannelRepositoryInterface */
-    private $repository;
+    private $paymentMethodRepository;
 
-    public function __construct(CacheItemPoolInterface $cache, SalesChannelRepositoryInterface $repository)
-    {
-        $this->cache      = $cache;
-        $this->repository = $repository;
+    /** @var EntityRepositoryInterface */
+    private $salesChannelRepository;
+
+    public function __construct(
+        CacheItemPoolInterface $cachePool,
+        SalesChannelRepositoryInterface $repository,
+        EntityRepositoryInterface $salesChannelRepository
+    ) {
+        $this->cachePool               = $cachePool;
+        $this->paymentMethodRepository = $repository;
+        $this->salesChannelRepository  = $salesChannelRepository;
     }
 
     public static function getSubscribedEvents(): array
     {
         return [
-            StorefrontRenderEvent::class => 'onRender',
+            StorefrontRenderEvent::class       => 'onRender',
+            EntityWrittenContainerEvent::class => 'onEntityWritten',
         ];
     }
 
-    public function onRender(StorefrontRenderEvent $event)
+    public function onRender(StorefrontRenderEvent $event): void
     {
-        $activePaymentMethods = $this->cache->getItem(self::CACHE_ID);
+        $cacheKey = $this->generateCacheKey(
+            $event->getSalesChannelContext()->getSalesChannel()->getId()
+        );
+
+        $activePaymentMethods = $this->cachePool->getItem($cacheKey);
 
         if ($activePaymentMethods->get() === null) {
-            $activePaymentMethods->set($this->collectActivePayonePaymentMethods($event->getSalesChannelContext()));
+            $activePaymentMethods->set(
+                $this->collectActivePayonePaymentMethods(
+                    $event->getSalesChannelContext()
+                )
+            );
 
-            $this->cache->save($activePaymentMethods);
+            $this->cachePool->save($activePaymentMethods);
         }
 
         $event->setParameter('activePaymentPaymentMethods', $activePaymentMethods->get());
+    }
+
+    public function onEntityWritten(EntityWrittenContainerEvent $event)
+    {
+        $ids = [];
+
+        $paymentMethodEvents = $event->getEventByDefinition(PaymentMethodDefinition::class);
+        if (null !== $paymentMethodEvents) {
+            $ids = array_merge($ids, $this->collectPrimaryKeys($paymentMethodEvents->getIds()));
+        }
+
+        $salesChannelEvents = $event->getEventByDefinition(SalesChannelPaymentMethodDefinition::class);
+        if (null !== $salesChannelEvents) {
+            $ids = array_merge($ids, $this->collectPrimaryKeys($salesChannelEvents->getIds()));
+        }
+
+        $clearCache = false;
+
+        foreach (PaymentMethodInstaller::paymentMethods as $paymentMethod) {
+            if (in_array($paymentMethod::UUID, $ids, true)) {
+                $clearCache = true;
+            }
+        }
+
+        if (!$clearCache) {
+            return;
+        }
+
+        $this->clearCache($event->getContext());
+    }
+
+    /**
+     * @param array|string $primaryKeys
+     *
+     * @return array
+     */
+    private function collectPrimaryKeys($primaryKeys): array
+    {
+        $ids = [];
+
+        foreach ($primaryKeys as $key) {
+            if (is_array($key)) {
+                $ids = array_merge($ids, array_values($key));
+            } else {
+                $ids[] = $key;
+            }
+        }
+
+        return $ids;
     }
 
     private function collectActivePayonePaymentMethods(SalesChannelContext $context): array
@@ -56,6 +127,27 @@ class StorefrontRenderEventListener implements EventSubscriberInterface
 
         $criteria->addFilter(new EqualsFilter('active', true));
 
-        return $this->repository->search($criteria, $context)->getIds();
+        return $this->paymentMethodRepository->search($criteria, $context)->getIds();
+    }
+
+    private function generateCacheKey(string $salesChannel): string
+    {
+        return 'payone_payment.menu_state.' . $salesChannel;
+    }
+
+    private function clearCache(Context $context): void
+    {
+        $cacheKeys     = [];
+        $salesChannels = $this->salesChannelRepository->searchIds(new Criteria(), $context)->getIds();
+
+        foreach ($salesChannels as $salesChannel) {
+            $cacheKeys[] = $this->generateCacheKey($salesChannel);
+        }
+
+        if (empty($cacheKeys)) {
+            return;
+        }
+
+        $this->cachePool->deleteItems($cacheKeys);
     }
 }
