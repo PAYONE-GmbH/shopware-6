7ba4c133dc257b70fbb5b501986cf30e0e6691b6
Uwe Kleinmann <uwe@kleinmann.org>
2019-08-14 19:43:23 +0200

    [PAYONE-47] Add first batch of test code, none of them doing anything yet.

---
+++ tests/Controller/WebhookControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayonePayment\Test\Controller;
+
+use PayonePayment\Controller\WebhookController;
+use PayonePayment\Payone\Webhook\Processor\WebhookProcessor;
+use PHPUnit\Framework\TestCase;
+use Shopware\Core\Checkout\Test\Cart\Common\Generator;
+use Shopware\Core\Defaults;
+use Shopware\Core\Framework\Context;
+use Symfony\Component\HttpFoundation\Request;
+
+class WebhookControllerTest extends TestCase
+{
+    public function testCreditcardAppointed(): void
+    {
+        $context             = Context::createDefaultContext();
+        $salesChannelContext = Generator::createSalesChannelContext($context);
+        $salesChannelContext->getSalesChannel()->setId(Defaults::SALES_CHANNEL);
+
+        $response = $this->createWebhookController()->execute(
+            new Request(),
+            $salesChannelContext
+        );
+    }
+
+    private function createWebhookController(): WebhookController
+    {
+        // TODO: Use mocks for dependencies of WebhookProcessor
+        return new WebhookController(
+            new WebhookProcessor()
+        );
+    }
+}
