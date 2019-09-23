ecc118477e495d32fd3a46ff3c4cbc77bfd43f6c
Jochen Manz <j.manz@kellerkinder.de>
2019-09-23 20:55:07 +0200

    [PAYONE-44] move storefront templates to a new storefront folder

--- src/PayonePayment.php
+++ src/PayonePayment.php
@@ -66,4 +66,12 @@ class PayonePayment extends Plugin
         $connection->exec('DROP TABLE payone_payment_redirect');
         $connection->exec('DROP TABLE payone_payment_mandate');
     }
+
+    public function getViewPaths(): array
+    {
+        return array_merge(parent::getViewPaths(), [
+            'Resources/views/storefront',
+            'Resources/views/administration',
+        ]);
+    }
 }
