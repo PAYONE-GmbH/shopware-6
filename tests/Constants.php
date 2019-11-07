bde901f2f003ae0ae4cf354cfbe08b6f99be7617
Uwe Kleinmann <uwe@kleinmann.org>
2019-11-07 11:04:22 +0100

    [PAYONE-47] Add webhook controller tests for different txaction values.

---
+++ tests/Constants.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayonePayment\Test;
+
+interface Constants
+{
+    public const CURRENCY_ID           = '9d185b6a82224319a326a0aed4f80d0a';
+    public const ORDER_ID              = 'c23b44f2778240c7ad09bee356004503';
+    public const ORDER_TRANSACTION_ID  = '4c8a04d0ae374bdbac305d717cdaf9c6';
+    public const PAYONE_TRANSACTION_ID = 'test-transaction-id';
+}
--- tests/Controller/WebhookControllerTest.php
+++ tests/Controller/WebhookControllerTest.php
@@ -5,38 +5,107 @@ declare(strict_types=1);
 namespace PayonePayment\Test\Controller;
 
 use PayonePayment\Controller\WebhookController;
+use PayonePayment\Payone\Webhook\Handler\WebhookHandlerInterface;
 use PayonePayment\Payone\Webhook\Processor\WebhookProcessor;
+use PayonePayment\Test\Constants;
 use PayonePayment\Test\Mock\Components\ConfigReaderMock;
+use PayonePayment\Test\Mock\Factory\TransactionStatusWebhookHandlerFactory;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\NullLogger;
+use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionStateHandler;
 use Shopware\Core\Checkout\Test\Cart\Common\Generator;
 use Shopware\Core\Defaults;
 use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
 use Symfony\Component\HttpFoundation\Request;
 
 class WebhookControllerTest extends TestCase
 {
+    /** @var MockObject&OrderTransactionStateHandler */
+    private $transactionStateHandler;
+
+    protected function setUp(): void
+    {
+        $this->transactionStateHandler = $this->createMock(OrderTransactionStateHandler::class);
+    }
+
     public function testCreditcardAppointed(): void
     {
-        $this->markTestIncomplete();
-
-        return;
-
         $context             = Context::createDefaultContext();
         $salesChannelContext = Generator::createSalesChannelContext($context);
         $salesChannelContext->getSalesChannel()->setId(Defaults::SALES_CHANNEL);
 
+        $this->transactionStateHandler->expects($this->once())->method('reopen')->with(Constants::ORDER_TRANSACTION_ID);
+
+        $request = new Request();
+        $request->request->set('key', md5(''));
+        $request->request->set('txid', Constants::PAYONE_TRANSACTION_ID);
+        $request->request->set('txaction', 'appointed');
+        $request->request->set('sequencenumber', '0');
+
         $response = $this->createWebhookController()->execute(
-            new Request(),
+            $request,
             $salesChannelContext
         );
+
+        $this->assertEquals(WebhookHandlerInterface::RESPONSE_TSOK, $response->getContent());
+    }
+
+    public function testCreditcardCapture(): void
+    {
+        $context             = Context::createDefaultContext();
+        $salesChannelContext = Generator::createSalesChannelContext($context);
+        $salesChannelContext->getSalesChannel()->setId(Defaults::SALES_CHANNEL);
+
+        $this->transactionStateHandler->expects($this->once())->method('pay')->with(Constants::ORDER_TRANSACTION_ID);
+
+        $request = new Request();
+        $request->request->set('key', md5(''));
+        $request->request->set('txid', Constants::PAYONE_TRANSACTION_ID);
+        $request->request->set('txaction', 'capture');
+        $request->request->set('receivable', '1');
+        $request->request->set('sequencenumber', '0');
+
+        $response = $this->createWebhookController()->execute(
+            $request,
+            $salesChannelContext
+        );
+
+        $this->assertEquals(WebhookHandlerInterface::RESPONSE_TSOK, $response->getContent());
+    }
+
+    public function testCreditcardPaid(): void
+    {
+        $context             = Context::createDefaultContext();
+        $salesChannelContext = Generator::createSalesChannelContext($context);
+        $salesChannelContext->getSalesChannel()->setId(Defaults::SALES_CHANNEL);
+
+        $this->transactionStateHandler->expects($this->once())->method('pay')->with(Constants::ORDER_TRANSACTION_ID);
+
+        $request = new Request();
+        $request->request->set('key', md5(''));
+        $request->request->set('txid', Constants::PAYONE_TRANSACTION_ID);
+        $request->request->set('txaction', 'paid');
+        $request->request->set('sequencenumber', '0');
+
+        $response = $this->createWebhookController()->execute(
+            $request,
+            $salesChannelContext
+        );
+
+        $this->assertEquals(WebhookHandlerInterface::RESPONSE_TSOK, $response->getContent());
     }
 
     private function createWebhookController(): WebhookController
     {
-        // TODO: Use mocks for dependencies of WebhookProcessor
+        $transactionStatusHandler = TransactionStatusWebhookHandlerFactory::createHandler(
+            $this->createMock(EntityRepository::class),
+            $this->transactionStateHandler
+        );
+
         return new WebhookController(
-            new WebhookProcessor(new ConfigReaderMock(), new \ArrayObject([]), new NullLogger())
+            new WebhookProcessor(new ConfigReaderMock(), new \ArrayObject([$transactionStatusHandler]), new NullLogger())
         );
     }
 }
