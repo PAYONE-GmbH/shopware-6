aa0317bf02a00dde5fffdf83868518bfc6aa076b
Fabian Böttcher <fabian.boettcher@payone.com>
2020-03-30 16:23:38 +0200

    Change credit card payment handler

---
+++ src/PaymentHandler/AbstractPayonePaymentHandler.php
@@ -0,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayonePayment\PaymentHandler;
+
+use PayonePayment\Components\ConfigReader\ConfigReaderInterface;
+use PayonePayment\Installer\CustomFieldInstaller;
+use PayonePayment\Payone\Client\Exception\PayoneRequestException;
+use PayonePayment\Payone\Client\PayoneClientInterface;
+use Shopware\Core\Checkout\Payment\Cart\AsyncPaymentTransactionStruct;
+use Shopware\Core\Checkout\Payment\Exception\AsyncPaymentProcessException;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Throwable;
+
+/**
+ * A base class for payment handlers which implements common processing
+ * steps of which child classes can make use of.
+ */
+abstract class AbstractPayonePaymentHandler implements PayonePaymentHandlerInterface
+{
+    /** @var ConfigReaderInterface */
+    protected $configReader;
+
+    /** @var PayoneClientInterface */
+    protected $client;
+
+    /** @var TranslatorInterface */
+    protected $translator;
+
+    public function __construct(
+        ConfigReaderInterface $configReader,
+        PayoneClientInterface $client,
+        TranslatorInterface $translator
+    ) {
+        $this->configReader = $configReader;
+        $this->client       = $client;
+        $this->translator   = $translator;
+    }
+
+    /**
+     * Returns the configured authorization method for this payment method.
+     *
+     * @param string $salesChannelId The ID of the associated sales channel.
+     * @param string $configKey The config key of the configured authorization method.
+     * @param string $default A default authorization method if no proper configuration can be found.
+     * @return string The authorization method to use for this payment process.
+     */
+    protected function getAuthorizationMethod(string $salesChannelId, string $configKey, string $default): string
+    {
+        $configuration              = $this->configReader->read($salesChannelId);
+        $defaultAuthorizationMethod = $configuration->get('authorizationMethod', $default);
+        $authorizationMethod        = $configuration->get($configKey, $defaultAuthorizationMethod);
+
+        return $authorizationMethod === 'default'
+            ? $defaultAuthorizationMethod
+            : $authorizationMethod;
+    }
+
+    /**
+     * Sends the provided request parameters to PAYONE and returns
+     * the received response parameters.
+     *
+     * @param array $request The request parameters to send.
+     * @param AsyncPaymentTransactionStruct $transaction The related transaction struct.
+     * @return array The response parameters.
+     * @throws AsyncPaymentProcessException If the payment fails for any reason.
+     */
+    protected function sendRequest(array $request, AsyncPaymentTransactionStruct $transaction): array
+    {
+        try {
+            return $this->client->request($request);
+        } catch (PayoneRequestException $exception) {
+            throw new AsyncPaymentProcessException(
+                $transaction->getOrderTransaction()->getId(),
+                $exception->getResponse()['error']['CustomerMessage']
+            );
+        } catch (Throwable $exception) {
+            throw new AsyncPaymentProcessException(
+                $transaction->getOrderTransaction()->getId(),
+                $this->translator->trans('PayonePayment.errorMessages.genericError')
+            );
+        }
+    }
+
+    /**
+     * Prepares and returns custom fields for the transaction.
+     *
+     * @param array $request The PAYONE request parameters.
+     * @param array $response The PAYONE response parameters.
+     * @param array $fields Any additional custom fields (higher priority).
+     * @return array A resulting array of custom fields for the transaction.
+     */
+    protected function prepareTransactionCustomFields(array $request, array $response, array $fields = []): array
+    {
+        return array_merge([
+            CustomFieldInstaller::AUTHORIZATION_TYPE => $request['request'],
+            CustomFieldInstaller::LAST_REQUEST       => $request['request'],
+            CustomFieldInstaller::TRANSACTION_ID     => (string) $response['txid'],
+            CustomFieldInstaller::TRANSACTION_STATE  => $response['status'],
+            CustomFieldInstaller::SEQUENCE_NUMBER    => -1,
+            CustomFieldInstaller::USER_ID            => $response['userid'],
+        ], $fields);
+    }
+}
---
+++ src/Payone/Request/CreditCard/AbstractCreditCardRequest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayonePayment\Payone\Request\CreditCard;
+
+use PayonePayment\Components\RedirectHandler\RedirectHandler;
+use PayonePayment\Struct\PaymentTransaction;
+use RuntimeException;
+use Shopware\Core\Checkout\Order\OrderEntity;
+use Shopware\Core\Checkout\Payment\Exception\InvalidOrderException;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\System\Currency\CurrencyEntity;
+
+abstract class AbstractCreditCardRequest
+{
+    /** @var RedirectHandler */
+    protected $redirectHandler;
+
+    /** @var EntityRepositoryInterface */
+    protected $currencyRepository;
+
+    public function __construct(
+        RedirectHandler $redirectHandler,
+        EntityRepositoryInterface $currencyRepository
+    ) {
+        $this->redirectHandler    = $redirectHandler;
+        $this->currencyRepository = $currencyRepository;
+    }
+
+    public function getRequestParameters(PaymentTransaction $transaction, Context $context, string $pseudoPan): array
+    {
+        if (empty($transaction->getReturnUrl())) {
+            throw new InvalidOrderException($transaction->getOrder()->getId());
+        }
+
+        $currency = $this->getOrderCurrency($transaction->getOrder(), $context);
+
+        return [
+            'clearingtype'  => 'cc',
+            'amount'        => (int) ($transaction->getOrder()->getAmountTotal() * (10 ** $currency->getDecimalPrecision())),
+            'currency'      => $currency->getIsoCode(),
+            'reference'     => $transaction->getOrder()->getOrderNumber(),
+            'pseudocardpan' => $pseudoPan,
+            'successurl'    => $this->redirectHandler->encode($transaction->getReturnUrl() . '&state=success'),
+            'errorurl'      => $this->redirectHandler->encode($transaction->getReturnUrl() . '&state=error'),
+            'backurl'       => $this->redirectHandler->encode($transaction->getReturnUrl() . '&state=cancel'),
+        ];
+    }
+
+    private function getOrderCurrency(OrderEntity $order, Context $context): CurrencyEntity
+    {
+        $criteria = new Criteria([$order->getCurrencyId()]);
+
+        /** @var null|CurrencyEntity $currency */
+        $currency = $this->currencyRepository->search($criteria, $context)->first();
+
+        if (null === $currency) {
+            throw new RuntimeException('missing order currency entity');
+        }
+
+        return $currency;
+    }
+}
