22b534f778ce680bb78ac5670414afdf2174b8b0
Fabian Böttcher <fabian.boettcher@payone.com>
2020-04-02 12:17:48 +0200

    Change Payolution debit payment handler

--- src/Payone/Request/PayolutionDebit/PayolutionDebitAuthorizeRequestFactory.php
+++ src/Payone/Request/PayolutionDebit/PayolutionDebitAuthorizeRequestFactory.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 namespace PayonePayment\Payone\Request\PayolutionDebit;
 
-class PayolutionDebitPreAuthorizeRequestFactory extends AbstractPayolutionDebitRequestFactory
+class PayolutionDebitAuthorizeRequestFactory extends AbstractPayolutionDebitRequestFactory
 {
 }
