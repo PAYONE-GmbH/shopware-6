ea86ee80116367ebbf7ca0d34e35be80d38a5c4f
Fabian Böttcher <fabian.boettcher@payone.com>
2020-06-16 16:47:56 +0200

    Integrate EPS payment

--- src/Installer/PaymentMethodInstaller.php
+++ src/Installer/PaymentMethodInstaller.php
@@ -8,6 +8,7 @@ use Doctrine\DBAL\Connection;
 use PayonePayment\PaymentMethod\PaymentMethodInterface;
 use PayonePayment\PaymentMethod\PayoneCreditCard;
 use PayonePayment\PaymentMethod\PayoneDebit;
+use PayonePayment\PaymentMethod\PayoneEps;
 use PayonePayment\PaymentMethod\PayonePayolutionDebit;
 use PayonePayment\PaymentMethod\PayonePayolutionInstallment;
 use PayonePayment\PaymentMethod\PayonePayolutionInvoicing;
@@ -37,6 +38,7 @@ class PaymentMethodInstaller implements InstallerInterface
         PayonePayolutionInvoicing::class,
         PayonePayolutionDebit::class,
         PayoneSofortBanking::class,
+        PayoneEps::class,
     ];
 
     /** @var PluginIdProvider */
---
+++ src/Payone/Request/Eps/EpsPreAuthorizeRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayonePayment\Payone\Request\Eps;
+
+use PayonePayment\Struct\PaymentTransaction;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
+
+class EpsPreAuthorizeRequest extends AbstractEpsAuthorizeRequest
+{
+    public function getRequestParameters(
+        PaymentTransaction $transaction,
+        RequestDataBag $dataBag,
+        Context $context
+    ): array {
+        return array_merge(parent::getRequestParameters($transaction, $dataBag, $context), [
+            'request' => 'preauthorization',
+        ]);
+    }
+}
