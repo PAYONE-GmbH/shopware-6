952714e95aa0e94b9b02a35a89725b78642616d9
Andre Gregor-Herrmann <a.gregor-herrmann@kellerkinder.de>
2020-08-27 20:29:54 +0200

    [PAYONE-78] Add request related parts for payment secure invoice

---
+++ src/Payone/Request/SecureInvoice/AbstractSecureInvoiceAuthorizeRequestFactory.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayonePayment\Payone\Request\SecureInvoice;
+
+use PayonePayment\Configuration\ConfigurationPrefixes;
+use PayonePayment\Payone\Request\AbstractRequestFactory;
+use PayonePayment\Payone\Request\Customer\CustomerRequest;
+use PayonePayment\Payone\Request\System\SystemRequest;
+use PayonePayment\Struct\PaymentTransaction;
+use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+
+
+abstract class AbstractSecureInvoiceAuthorizeRequestFactory extends AbstractRequestFactory
+{
+    /** @var AbstractSecureInvoiceAuthorizeRequest */
+    private $secureInvoiceRequest;
+
+    /** @var CustomerRequest */
+    private $customerRequest;
+
+    /** @var SystemRequest */
+    private $systemRequest;
+
+    public function __construct(
+        AbstractSecureInvoiceAuthorizeRequest $secureInvoiceAuthorizeRequest,
+        CustomerRequest $customerRequest,
+        SystemRequest $systemRequest
+    ) {
+        $this->secureInvoiceRequest       = $secureInvoiceAuthorizeRequest;
+        $this->customerRequest            = $customerRequest;
+        $this->systemRequest              = $systemRequest;
+    }
+
+    public function getRequestParameters(
+        PaymentTransaction $transaction,
+        RequestDataBag $dataBag,
+        SalesChannelContext $context
+    ): array {
+        $this->requests[] = $this->systemRequest->getRequestParameters(
+            $transaction->getOrder()->getSalesChannelId(),
+            ConfigurationPrefixes::CONFIGURATION_PREFIX_PAYOLUTION_INVOICING,
+            $context->getContext()
+        );
+
+        $this->requests[] = $this->customerRequest->getRequestParameters(
+            $context
+        );
+
+        $referenceNumber = $this->systemRequest->getReferenceNumber($transaction, true);
+
+        $this->requests[] = $this->secureInvoiceRequest->getRequestParameters(
+            $transaction,
+            $dataBag,
+            $context,
+            $referenceNumber
+        );
+
+        return $this->createRequest();
+    }
+}
---
+++ src/Payone/Request/SecureInvoice/SecureInvoiceAuthorizeRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayonePayment\Payone\Request\SecureInvoice;
+
+use PayonePayment\Struct\PaymentTransaction;
+use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+
+class SecureInvoiceAuthorizeRequest extends AbstractSecureInvoiceAuthorizeRequest
+{
+    public function getRequestParameters(
+        PaymentTransaction $transaction,
+        RequestDataBag $dataBag,
+        SalesChannelContext $context,
+        string $referenceNumber
+    ): array {
+        return array_merge(parent::getRequestParameters($transaction, $dataBag, $context, $referenceNumber), [
+            'request' => 'authorization',
+        ]);
+    }
+}
