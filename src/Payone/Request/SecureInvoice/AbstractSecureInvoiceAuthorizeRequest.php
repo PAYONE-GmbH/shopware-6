07d56184cb105692072c72788c55aa50c3b60d4c
a.wink <a.wink@kellerkinder.de>
2020-09-16 15:12:06 +0200

    [PAYONE-80] Extract LineItemHydrator to own class

--- src/Payone/Request/SecureInvoice/AbstractSecureInvoiceAuthorizeRequest.php
+++ src/Payone/Request/SecureInvoice/AbstractSecureInvoiceAuthorizeRequest.php
@@ -5,16 +5,12 @@ declare(strict_types=1);
 namespace PayonePayment\Payone\Request\SecureInvoice;
 
 use DateTime;
-use Exception;
+use PayonePayment\Components\Hydrator\LineItemHydrator\LineItemHydratorInterface;
 use PayonePayment\PaymentMethod\PayoneSecureInvoice;
 use PayonePayment\Struct\PaymentTransaction;
 use RuntimeException;
-use Shopware\Core\Checkout\Cart\LineItem\LineItem;
 use Shopware\Core\Checkout\Order\Aggregate\OrderAddress\OrderAddressEntity;
-use Shopware\Core\Checkout\Order\Aggregate\OrderLineItem\OrderLineItemCollection;
-use Shopware\Core\Checkout\Order\Aggregate\OrderLineItem\OrderLineItemEntity;
 use Shopware\Core\Checkout\Order\OrderEntity;
-use Shopware\Core\Checkout\Promotion\Cart\PromotionProcessor;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
@@ -24,8 +20,8 @@ use Shopware\Core\System\SalesChannel\SalesChannelContext;
 
 abstract class AbstractSecureInvoiceAuthorizeRequest
 {
-    public const TYPE_GOODS   = 'goods';
-    public const TYPE_VOUCHER = 'voucher';
+    /** @var LineItemHydratorInterface */
+    private $lineItemHydrator;
 
     /** @var EntityRepositoryInterface */
     private $currencyRepository;
@@ -34,9 +30,11 @@ abstract class AbstractSecureInvoiceAuthorizeRequest
     private $orderAddressRepository;
 
     public function __construct(
+        LineItemHydratorInterface $lineItemHydrator,
         EntityRepositoryInterface $currencyRepository,
         EntityRepositoryInterface $orderAddressRepository
     ) {
+        $this->lineItemHydrator       = $lineItemHydrator;
         $this->currencyRepository     = $currencyRepository;
         $this->orderAddressRepository = $orderAddressRepository;
     }
@@ -75,7 +73,7 @@ abstract class AbstractSecureInvoiceAuthorizeRequest
             $parameters['company'] = $billingAddress->getCompany();
         }
 
-        $parameters = array_merge($parameters, $this->mapOrderLines($currency, $order->getLineItems()));
+        $parameters = array_merge($parameters, $this->lineItemHydrator->mapOrderLines($currency, $order->getLineItems()));
 
         if (!$company && !empty($dataBag->get('secureInvoiceBirthday'))) {
             $birthday = DateTime::createFromFormat('Y-m-d', $dataBag->get('secureInvoiceBirthday'));
@@ -86,59 +84,6 @@ abstract class AbstractSecureInvoiceAuthorizeRequest
         }
 
         return array_filter($parameters);
-    }
-
-    protected function mapOrderLines(CurrencyEntity $currency, OrderLineItemCollection $lineItemCollection): array
-    {
-        $requestLineItems = [];
-
-        /** @var OrderLineItemEntity $lineItem */
-        foreach ($lineItemCollection as $lineItem) {
-            try {
-                /** @phpstan-ignore-next-line */
-                if (class_exists('Swag\CustomizedProducts\Core\Checkout\CustomizedProductsCartDataCollector') &&
-                    CustomizedProductsCartDataCollector::CUSTOMIZED_PRODUCTS_TEMPLATE_LINE_ITEM_TYPE === $lineItem->getType(
-                    ) &&
-                    null === $lineItem->getParentId()) {
-                    continue;
-                }
-            } catch (Exception $exception) {
-                // Catch class not found if SwagCustomizedProducts plugin is not installed
-            }
-
-            $taxes = $lineItem->getPrice() ? $lineItem->getPrice()->getCalculatedTaxes() : null;
-
-            if (null === $taxes || null === $taxes->first()) {
-                continue;
-            }
-
-            $counter                                  = count($requestLineItems) + 1;
-            $requestLineItems['it[' . $counter . ']'] = $this->mapItemType($lineItem->getType());
-            $requestLineItems['id[' . $counter . ']'] = $lineItem->getIdentifier();
-            $requestLineItems['pr[' . $counter . ']'] = (int) round(
-                ($lineItem->getUnitPrice() * (10 ** $currency->getDecimalPrecision()))
-            );
-            $requestLineItems['no[' . $counter . ']'] = $lineItem->getQuantity();
-            $requestLineItems['de[' . $counter . ']'] = $lineItem->getLabel();
-            $requestLineItems['va[' . $counter . ']'] = (int) round(
-                ($taxes->first()->getTaxRate() * (10 ** $currency->getDecimalPrecision()))
-            );
-        }
-
-        return $requestLineItems;
-    }
-
-    protected function mapItemType(?string $itemType): string
-    {
-        if ($itemType === LineItem::CREDIT_LINE_ITEM_TYPE) {
-            return self::TYPE_VOUCHER;
-        }
-
-        if ($itemType === PromotionProcessor::LINE_ITEM_TYPE) {
-            return self::TYPE_VOUCHER;
-        }
-
-        return self::TYPE_GOODS;
     }
 
     private function getOrderCurrency(OrderEntity $order, Context $context): CurrencyEntity
