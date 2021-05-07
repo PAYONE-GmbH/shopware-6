aaa85bb3115c00246a3f899a86fd83bf756609ec
Stephan Altmann <s.altmann@kellerkinder.de>
2021-05-07 20:14:17 +0200

    [PAYONE-95] add @refactor for loginRequired workaround

--- src/Storefront/Controller/Account/AccountMandateController.php
+++ src/Storefront/Controller/Account/AccountMandateController.php
@@ -80,6 +80,8 @@ class AccountMandateController extends StorefrontController
     }
 
     /**
+     * @refactor Implementing core legacy code is imho not the best solution
+     *
      * This method has been removed with SW 6.4. It is recommended to use LoginRequired annotation instead.
      * The annotation is not supported by SW 6.2.
      *
