f3d605a4a738e8ff6375e65a2f2713c21dc68449
Stephan Altmann <s.altmann@kellerkinder.de>
2021-06-15 08:47:59 +0200

    [PAYONE-99] import rules

--- src/Resources/app/administration/src/acl/index.js
+++ src/Resources/app/administration/src/acl/index.js
@@ -8,32 +8,8 @@ try {
                 privileges: [
                     'order_transaction:update',
                     'order_line_item:update',
-                    'order.viewer',
-                    'order:read',
-                    'user_config:read',
                     'state_machine_history:create',
-                    'order_address:read',
-                    'sales_channel:read',
-                    'order_customer:read',
-                    'currency:read',
-                    'document:read',
-                    'order_transaction:read',
-                    'order_delivery:read',
-                    'order_line_item:read',
-                    'shipping_method:read',
-                    'country:read',
-                    'country_state:read',
-                    'payment_method:read',
-                    'document_type:read',
-                    'tag:read',
-                    'custom_field_set:read',
-                    'custom_field:read',
-                    'custom_field_set_relation:read',
-                    'state_machine_history:read',
-                    'state_machine_state:read',
-                    'user:read',
-                    'state_machine_state:read',
-                    'state_machine:read'
+                    Shopware.Service('privileges').getPrivileges('order.viewer')
                 ],
                 dependencies: []
             }
