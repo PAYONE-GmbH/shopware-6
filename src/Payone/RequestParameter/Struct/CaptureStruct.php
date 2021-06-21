15b6f2c83abf2421c0d88c08a51de62645594494
Stephan Altmann <s.altmann@kellerkinder.de>
2021-06-21 15:49:22 +0200

    [PAYONE-87] implement capture request builder

---
+++ src/Payone/RequestParameter/Struct/CaptureStruct.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayonePayment\Payone\RequestParameter\Struct;
+
+use PayonePayment\Payone\RequestParameter\Struct\Traits\DeterminationTrait;
+use PayonePayment\Payone\RequestParameter\Struct\Traits\TransactionTrait;
+use PayonePayment\Struct\PaymentTransaction;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\Struct\Struct;
+
+class CaptureStruct extends Struct
+{
+    use DeterminationTrait;
+    use TransactionTrait;
+
+    /** @var Context */
+    protected $context;
+
+    /** @var array */
+    protected $customFields;
+
+    /** @var null|float */
+    protected $totalAmount;
+
+    /** @var bool */
+    protected $completed = false;
+
+    public function __construct(PaymentTransaction $paymentTransaction, Context $context, array $customFields, ?float $totalAmount = null, bool $completed = false)
+    {
+        $this->paymentTransaction = $paymentTransaction;
+        $this->context            = $context;
+        $this->customFields       = $customFields;
+        $this->totalAmount        = $totalAmount;
+        $this->completed          = $completed;
+    }
+
+    public function getContext(): Context
+    {
+        return $this->context;
+    }
+
+    public function setContext(Context $context): void
+    {
+        $this->context = $context;
+    }
+
+    public function getCustomFields(): array
+    {
+        return $this->customFields;
+    }
+
+    public function setCustomFields(array $customFields): void
+    {
+        $this->customFields = $customFields;
+    }
+
+    public function getTotalAmount(): ?float
+    {
+        return $this->totalAmount;
+    }
+
+    public function setTotalAmount(?float $totalAmount): void
+    {
+        $this->totalAmount = $totalAmount;
+    }
+
+    public function isCompleted(): bool
+    {
+        return $this->completed;
+    }
+
+    public function setCompleted(bool $completed): void
+    {
+        $this->completed = $completed;
+    }
+}
---
+++ src/Payone/RequestParameter/Struct/Traits/TransactionTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayonePayment\Payone\RequestParameter\Struct\Traits;
+
+use PayonePayment\Struct\PaymentTransaction;
+
+trait TransactionTrait
+{
+    /** @var PaymentTransaction */
+    protected $paymentTransaction;
+
+    public function getPaymentTransaction(): PaymentTransaction
+    {
+        return $this->paymentTransaction;
+    }
+
+    public function setPaymentTransaction(PaymentTransaction $paymentTransaction): void
+    {
+        $this->paymentTransaction = $paymentTransaction;
+    }
+}
