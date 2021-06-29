3844c9b1a0686dbd3395c2bfc7a94ee32a6f9b9c
Stephan Altmann <s.altmann@kellerkinder.de>
2021-06-29 13:22:55 +0200

    [PAYONE-88] deactivate payone payment methods on zero amount carts

---
+++ src/Components/CartValidator/ZeroAmountCartValidator.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayonePayment\Components\CartValidator;
+
+use Shopware\Core\Checkout\Cart\Cart;
+use Shopware\Core\Checkout\Cart\CartValidatorInterface;
+use Shopware\Core\Checkout\Cart\Error\ErrorCollection;
+use Shopware\Core\Checkout\Payment\Cart\Error\PaymentMethodBlockedError;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+
+class ZeroAmountCartValidator implements CartValidatorInterface
+{
+    public function validate(Cart $cart, ErrorCollection $errors, SalesChannelContext $context): void
+    {
+        if (((int) round($cart->getPrice()->getTotalPrice() * (10 ** $context->getCurrency()->getDecimalPrecision()))) > 0) {
+            return;
+        }
+
+        if (mb_strpos($context->getPaymentMethod()->getHandlerIdentifier(), 'PayonePayment') === false) {
+            return;
+        }
+
+        $errors->add(
+            new PaymentMethodBlockedError((string) $context->getPaymentMethod()->getTranslation('name'))
+        );
+    }
+}
