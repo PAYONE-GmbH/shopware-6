b83f34414df519cef062d4b104d4a03fa5d447b8
Stephan Altmann <s.altmann@kellerkinder.de>
2021-08-10 16:44:32 +0200

    [PAYONE-65] implement mandate route

--- src/Storefront/Page/Mandate/AccountMandatePageLoader.php
+++ src/Storefront/Page/Mandate/AccountMandatePageLoader.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace PayonePayment\Storefront\Page\Mandate;
 
-use PayonePayment\Components\MandateService\MandateServiceInterface;
+use PayonePayment\StoreApi\Route\AbstractMandateRoute;
 use Shopware\Core\Checkout\Cart\Exception\CustomerNotLoggedInException;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Page\GenericPageLoader;
@@ -19,17 +19,17 @@ class AccountMandatePageLoader
     /** @var EventDispatcherInterface */
     private $eventDispatcher;
 
-    /** @var MandateServiceInterface */
-    private $mandateService;
+    /** @var AbstractMandateRoute */
+    private $mandateRoute;
 
     public function __construct(
         GenericPageLoader $genericLoader,
         EventDispatcherInterface $eventDispatcher,
-        MandateServiceInterface $MandateService
+        AbstractMandateRoute $mandateRoute
     ) {
         $this->genericLoader   = $genericLoader;
         $this->eventDispatcher = $eventDispatcher;
-        $this->mandateService  = $MandateService;
+        $this->mandateRoute    = $mandateRoute;
     }
 
     public function load(Request $request, SalesChannelContext $context): AccountMandatePage
@@ -42,14 +42,11 @@ class AccountMandatePageLoader
             $this->genericLoader->load($request, $context)
         );
 
-        if (null !== $context->getCustomer()) {
-            $page->setMandates(
-                $this->mandateService->getMandates(
-                    $context->getCustomer(),
-                    $context
-                )
-            );
-        }
+        $page->setMandates(
+            $this->mandateRoute->load(
+                $context
+            )->getSearchResult()
+        );
 
         $this->eventDispatcher->dispatch(
             new AccountMandatePageLoadedEvent($page, $context, $request)
