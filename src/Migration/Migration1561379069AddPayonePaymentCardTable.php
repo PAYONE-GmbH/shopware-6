1a2e7b1b18d75585d15206dc86096bccda30f940
Stephan Altmann <s.altmann@kellerkinder.de>
2021-08-12 11:23:06 +0200

    [PAYONE-105] update migrations

--- src/Migration/Migration1561379069AddPayonePaymentCardTable.php
+++ src/Migration/Migration1561379069AddPayonePaymentCardTable.php
@@ -16,30 +16,41 @@ class Migration1561379069AddPayonePaymentCardTable extends MigrationStep
 
     public function update(Connection $connection): void
     {
-        $connection->exec('            
+        $sql = '
             CREATE TABLE `payone_payment_card` (
                 `id` BINARY(16) NOT NULL,
-                
+
                 `customer_id` BINARY(16) NOT NULL,
-                
+
                 `pseudo_card_pan` VARCHAR(255) NOT NULL,
                 `truncated_card_pan` VARCHAR(255) NOT NULL,
                 `expires_at` DATETIME NOT NULL,
-                
+
                 `created_at` DATETIME(3) NOT NULL,
                 `updated_at` DATETIME(3) NULL,
-                
+
                 PRIMARY KEY (`id`),
-                
+
                 KEY `idx.payone_payment_card.expires_at` (`expires_at`),
                 KEY `fk.payone_payment_card.customer_id` (`customer_id`),
-                
-                CONSTRAINT `fk.payone_payment_card.customer_id` 
-                    FOREIGN KEY (`customer_id`) 
-                    REFERENCES `customer` (`id`) 
+
+                CONSTRAINT `fk.payone_payment_card.customer_id`
+                    FOREIGN KEY (`customer_id`)
+                    REFERENCES `customer` (`id`)
                     ON DELETE RESTRICT ON UPDATE CASCADE
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
-        ');
+        ';
+
+        if (method_exists($connection, 'executeStatement')) {
+            $connection->executeStatement($sql);
+
+            return;
+        }
+
+        if (method_exists($connection, 'exec')) {
+            /** @noinspection PhpDeprecationInspection */
+            $connection->exec($sql);
+        }
     }
 
     public function updateDestructive(Connection $connection): void
