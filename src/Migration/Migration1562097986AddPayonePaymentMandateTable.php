625826efee2e8d1b833c1faed6016e18c8d144e5
Stephan Altmann <s.altmann@kellerkinder.de>
2021-08-12 11:23:06 +0200

    [PAYONE-105] update migrations

--- src/Migration/Migration1562097986AddPayonePaymentMandateTable.php
+++ src/Migration/Migration1562097986AddPayonePaymentMandateTable.php
@@ -16,28 +16,39 @@ class Migration1562097986AddPayonePaymentMandateTable extends MigrationStep
 
     public function update(Connection $connection): void
     {
-        $connection->exec('            
+        $sql = '
             CREATE TABLE `payone_payment_mandate` (
                 `id` BINARY(16) NOT NULL,
-                
+
                 `customer_id` BINARY(16) NOT NULL,
-                
+
                 `identification` VARCHAR(255) NOT NULL,
                 `signature_date` datetime(3) NOT NULL,
-                
+
                 `created_at` DATETIME(3) NOT NULL,
                 `updated_at` DATETIME(3) NULL,
-                
+
                 PRIMARY KEY (`id`),
-                
+
                 KEY `fk.payone_payment_mandate.customer_id` (`customer_id`),
-                
-                CONSTRAINT `fk.payone_payment_mandate.customer_id` 
-                    FOREIGN KEY (`customer_id`) 
-                    REFERENCES `customer` (`id`) 
+
+                CONSTRAINT `fk.payone_payment_mandate.customer_id`
+                    FOREIGN KEY (`customer_id`)
+                    REFERENCES `customer` (`id`)
                     ON DELETE RESTRICT ON UPDATE CASCADE
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
-        ');
+        ';
+
+        if (method_exists($connection, 'executeStatement')) {
+            $connection->executeStatement($sql);
+
+            return;
+        }
+
+        if (method_exists($connection, 'exec')) {
+            /** @noinspection PhpDeprecationInspection */
+            $connection->exec($sql);
+        }
     }
 
     public function updateDestructive(Connection $connection): void
