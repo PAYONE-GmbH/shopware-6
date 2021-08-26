98b271d3a54f35faa64d4473eb83642399b9f12b
Stephan Altmann <s.altmann@kellerkinder.de>
2021-08-26 14:36:04 +0200

    [PAYONE-104] implement complete merchant validation

--- src/StoreApi/Route/ApplePayRoute.php
+++ src/StoreApi/Route/ApplePayRoute.php
@@ -12,10 +12,12 @@ use Shopware\Core\Framework\Plugin\Exception\DecorationPatternException;
 use Shopware\Core\Framework\Routing\Annotation\ContextTokenRequired;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Throwable;
 
 /**
  * @RouteScope(scopes={"store-api"})
@@ -32,6 +34,7 @@ class ApplePayRoute extends AbstractApplePayRoute
     public function __construct(Client $httpClient, LoggerInterface $logger)
     {
         $this->httpClient = $httpClient;
+        $this->logger = $logger;
     }
 
     public function getDecorated(): AbstractCardRoute
@@ -45,6 +48,11 @@ class ApplePayRoute extends AbstractApplePayRoute
     public function validateMerchant(Request $request, SalesChannelContext $context): Response
     {
         $validationUrl = $request->get('validationUrl', 'https://apple-pay-gateway.apple.com/paymentservices/paymentSession');
+        $merchantIdCertPath = __DIR__ . '/../../apple-pay-cert/merchant_id.pem';
+        $merchantIdKeyPath = __DIR__ . '/../../apple-pay-cert/merchant_id.key';
+
+        //TODO: get from config
+        $passPhrase = '';
 
         //TODO: get from config
         $body = [
@@ -54,16 +62,38 @@ class ApplePayRoute extends AbstractApplePayRoute
             'initiativeContext' => 'saltmann-payone-kellerkinder-io.eu.ngrok.io',
         ];
 
-        //TODO: add notice in config for certs
-        $response = $this->httpClient->request('POST', $validationUrl,
-            [
-                'json' => $body,
-                'cert'  => __DIR__ . '/../../apple-pay-cert/merchant_id.pem',
-                'ssl_key'    => __DIR__ . '/../../apple-pay-cert/merchant_id.key',
-            ]
-        );
+        if(!file_exists($merchantIdCertPath) || !file_exists($merchantIdKeyPath)) {
+            $this->logger->error('ApplePay MerchantValidation Cert files missing.', [ $merchantIdCertPath, $merchantIdKeyPath ]);
+            throw new FileNotFoundException();
+        }
 
-        return new JsonResponse($response->getBody(), $response->getStatusCode(), $response->getHeaders());
+        //TODO: add notice in config for certs
+        //TODO: config passphrase
+        try {
+            $response = $this->httpClient->request('POST', $validationUrl,
+                [
+                    'json' => $body,
+                    'cert' => (empty($passPhrase)) ? $merchantIdCertPath : [$merchantIdCertPath, $passPhrase],
+                    'ssl_key' => $merchantIdKeyPath,
+                    'http_errors' => false
+                ]
+            );
+        } catch(Throwable $e) {
+            $this->logger->error('ApplePay merchant validation failed.', [ 'body' => $body, 'message' =>  $e->getMessage(), 'url' => $validationUrl ]);
+            return new Response(null, 500);
+        }
+
+        $statusCode = $response->getStatusCode();
+
+        if($statusCode !== 200) {
+            $this->logger->error('ApplePay merchant validation failed.', [ 'body' => $body, 'statusCode' =>  $statusCode, 'url' => $validationUrl ]);
+        }
+
+        return new JsonResponse(
+            json_decode($response->getBody()->getContents(), true),
+            $statusCode,
+            $response->getHeaders()
+        );
     }
 
     /**
