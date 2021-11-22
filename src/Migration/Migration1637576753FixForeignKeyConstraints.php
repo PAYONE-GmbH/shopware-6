3078378d41ba6a9e6a6a869c48e4b8a9f498b5cf
Stephan Altmann <s.altmann@kellerkinder.de>
2021-11-22 12:01:41 +0100

    [PAYONE-124] change foreign key for payone payment card table

---
+++ src/Migration/Migration1637576753FixForeignKeyConstraints.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace PayonePayment\Migration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+class Migration1637576753FixForeignKeyConstraints extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1637576753;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $sql = 'ALTER TABLE payone_payment_card DROP FOREIGN KEY `fk.payone_payment_card.customer_id`;';
+
+        if (method_exists($connection, 'executeStatement')) {
+            $connection->executeStatement($sql);
+
+            return;
+        }
+
+        if (method_exists($connection, 'exec')) {
+            /** @noinspection PhpDeprecationInspection */
+            $connection->exec($sql);
+        }
+
+        $sql = 'ALTER TABLE payone_payment_card ADD FOREIGN KEY (`fk.payone_payment_card.customer_id`)
+                REFERENCES `customer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;';
+
+        if (method_exists($connection, 'executeStatement')) {
+            $connection->executeStatement($sql);
+
+            return;
+        }
+
+        if (method_exists($connection, 'exec')) {
+            /** @noinspection PhpDeprecationInspection */
+            $connection->exec($sql);
+        }
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+        // implement update destructive
+    }
+}
