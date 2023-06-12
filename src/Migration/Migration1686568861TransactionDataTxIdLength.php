3db088f1e894648733f82d99b8d32a9f8a015422
Frederik Rommel <frederik.rommel@webidea24.de>
2023-06-12 13:36:55 +0200

    PAYOSWXP-107: add index for transaction-id in transaction-data table for better performance

---
+++ src/Migration/Migration1686568861TransactionDataTxIdLength.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayonePayment\Migration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+class Migration1686568861TransactionDataTxIdLength extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1686568861;
+    }
+
+    public function update(Connection $connection): void
+    {
+        // reduce max length of transaction-id
+        $sql = 'ALTER TABLE `payone_payment_order_transaction_data` CHANGE `transaction_id` `transaction_id` VARCHAR(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;';
+
+        if (method_exists($connection, 'executeStatement')) {
+            $connection->executeStatement($sql);
+
+            return;
+        }
+
+        if (method_exists($connection, 'exec')) {
+            /** @noinspection PhpDeprecationInspection */
+            $connection->exec($sql);
+        }
+    }
+
+    public function updateDestructive(Connection $connection): void
+    {
+        // implement update destructive
+    }
+}
