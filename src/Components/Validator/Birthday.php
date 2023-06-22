bc39418511c483f9da43b2ce80b3293c669f37de
Frederik Rommel <frederik.rommel@webidea24.de>
2023-06-22 18:50:29 +0200

    fix phpstan issues

--- src/Components/Validator/Birthday.php
+++ src/Components/Validator/Birthday.php
@@ -14,15 +14,12 @@ class Birthday extends AbstractComparison
 {
     final public const TOO_HIGH_ERROR = 'ac2f93c6-f906-47c9-8b09-6f7cf41f4f49';
 
+    protected const ERROR_NAMES = [
+        self::TOO_HIGH_ERROR => 'PAYONE_BIRTHDAY_NOT_VALID',
+    ];
+
     /**
      * @phpstan-ignore-next-line
      */
     public $message = 'This value should be less than or equal to {{ compared_value }}.';
-
-    /**
-     * @var string[]
-     */
-    protected static $errorNames = [
-        self::TOO_HIGH_ERROR => 'PAYONE_BIRTHDAY_NOT_VALID',
-    ];
 }
