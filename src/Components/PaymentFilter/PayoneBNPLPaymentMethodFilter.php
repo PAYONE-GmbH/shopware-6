fcc0de67334f337abf63536d7a8e0fa03013429a
Moritz Müller <moritz.mueller@interlutions.de>
2024-01-14 16:26:16 +0100

    [PAYOSWXP-113] Add configuration to allow different shipping address for BNPL payments

--- src/Components/PaymentFilter/PayoneBNPLPaymentMethodFilter.php
+++ src/Components/PaymentFilter/PayoneBNPLPaymentMethodFilter.php
@@ -4,13 +4,34 @@ declare(strict_types=1);
 
 namespace PayonePayment\Components\PaymentFilter;
 
+use PayonePayment\Components\ConfigReader\ConfigReader;
 use PayonePayment\Core\Utils\AddressCompare;
 use Shopware\Core\Checkout\Customer\Aggregate\CustomerAddress\CustomerAddressEntity;
 use Shopware\Core\Checkout\Order\Aggregate\OrderAddress\OrderAddressEntity;
 use Shopware\Core\Checkout\Payment\PaymentMethodCollection;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
 
 class PayoneBNPLPaymentMethodFilter extends DefaultPaymentFilterService
 {
+    public function __construct(
+        private readonly SystemConfigService $systemConfigService,
+        private readonly string $paymentHandlerClass,
+        ?array $allowedCountries = null,
+        ?array $allowedB2bCountries = null,
+        ?array $allowedCurrencies = null,
+        float $allowedMinValue = 0.0,
+        ?float $allowedMaxValue = null,
+    ) {
+        parent::__construct(
+            $paymentHandlerClass,
+            $allowedCountries,
+            $allowedB2bCountries,
+            $allowedCurrencies,
+            $allowedMinValue,
+            $allowedMaxValue
+        );
+    }
+
     public function filterPaymentMethods(PaymentMethodCollection $methodCollection, PaymentFilterContext $filterContext): PaymentMethodCollection
     {
         $methodCollection = parent::filterPaymentMethods($methodCollection, $filterContext);
@@ -20,10 +41,19 @@ class PayoneBNPLPaymentMethodFilter extends DefaultPaymentFilterService
             return $methodCollection;
         }
 
+        $differentShippingAddressAllowed = (bool) $this->systemConfigService->get(
+            ConfigReader::getConfigKeyByPaymentHandler($this->paymentHandlerClass, 'AllowDifferentShippingAddress'),
+            $filterContext->getSalesChannelContext()->getSalesChannelId()
+        );
+
+        if ($differentShippingAddressAllowed) {
+            return $methodCollection;
+        }
+
         $billingAddress = $filterContext->getBillingAddress();
         $shippingAddress = $filterContext->getShippingAddress();
 
-        // Different billing and shipping addresses are not allowed for secured invoice
+        // Different billing and shipping addresses are not allowed
         if ($billingAddress instanceof OrderAddressEntity
             && $shippingAddress instanceof OrderAddressEntity
             && !AddressCompare::areOrderAddressesIdentical($billingAddress, $shippingAddress)) {
