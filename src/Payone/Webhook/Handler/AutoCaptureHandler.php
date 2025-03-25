4074b83d7371a3bc13367ace1097fba303e06e0d
Moritz Müller <moritz@momocode.de>
2025-03-25 20:12:51 +0100

    Change webhook for prepayment auto capture to 'paid'
    PAYOSWXP-168

--- src/Payone/Webhook/Handler/AutoCaptureHandler.php
+++ src/Payone/Webhook/Handler/AutoCaptureHandler.php
@@ -7,6 +7,7 @@ namespace PayonePayment\Payone\Webhook\Handler;
 use PayonePayment\Components\AutomaticCaptureService\AutomaticCaptureServiceInterface;
 use PayonePayment\Components\DataHandler\Transaction\TransactionDataHandlerInterface;
 use PayonePayment\Components\TransactionStatus\TransactionStatusService;
+use PayonePayment\PaymentMethod\PayonePrepayment;
 use PayonePayment\Struct\PaymentTransaction;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Symfony\Component\HttpFoundation\Request;
@@ -30,11 +31,25 @@ class AutoCaptureHandler implements WebhookHandlerInterface
             return;
         }
 
-        $this->automaticCaptureService->captureIfPossible($paymentTransaction, $salesChannelContext);
+        $txAction = $request->request->get('txaction');
+        $paymentMethodId = $paymentTransaction->getOrderTransaction()->getPaymentMethodId();
+
+        // For prepayment, capturing is only permitted for “paid” webhooks. For other payment methods, only the “appointed” webhook may be captured.
+        if (($txAction === TransactionStatusService::ACTION_PAID && $paymentMethodId === PayonePrepayment::UUID)
+            || ($txAction === TransactionStatusService::ACTION_APPOINTED && $paymentMethodId !== PayonePrepayment::UUID)) {
+            $this->automaticCaptureService->captureIfPossible($paymentTransaction, $salesChannelContext);
+        }
     }
 
     public function supports(SalesChannelContext $salesChannelContext, array $data): bool
     {
-        return isset($data['txid']) && ($data['txaction'] ?? null) === TransactionStatusService::ACTION_APPOINTED;
+        return isset($data['txid']) && \in_array(
+            ($data['txaction'] ?? null),
+            [
+                TransactionStatusService::ACTION_APPOINTED,
+                TransactionStatusService::ACTION_PAID,
+            ],
+            true
+        );
     }
 }
--- tests/Payone/Webhook/Handler/AutoCaptureHandlerTest.php
+++ tests/Payone/Webhook/Handler/AutoCaptureHandlerTest.php
@@ -7,8 +7,11 @@ namespace PayonePayment\Payone\Webhook\Handler;
 use PayonePayment\Components\AutomaticCaptureService\AutomaticCaptureServiceInterface;
 use PayonePayment\Components\DataHandler\Transaction\TransactionDataHandlerInterface;
 use PayonePayment\Components\TransactionStatus\TransactionStatusService;
+use PayonePayment\PaymentMethod\PayoneOpenInvoice;
+use PayonePayment\PaymentMethod\PayonePrepayment;
 use PayonePayment\Struct\PaymentTransaction;
 use PHPUnit\Framework\TestCase;
+use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionEntity;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -26,9 +29,10 @@ class AutoCaptureHandlerTest extends TestCase
 
         $salesChannelMock = $this->createMock(SalesChannelContext::class);
 
-        static::assertTrue($handler->supports($salesChannelMock, ['txid' => 1, 'txaction' => 'appointed']), 'supports should return true, cause of valid txid nd correct status');
+        static::assertTrue($handler->supports($salesChannelMock, ['txid' => 1, 'txaction' => 'paid']), 'supports should return true, cause of valid txid and correct status');
+        static::assertTrue($handler->supports($salesChannelMock, ['txid' => 1, 'txaction' => 'appointed']), 'supports should return true, cause of valid txid and correct status');
 
-        static::assertFalse($handler->supports($salesChannelMock, ['txaction' => 'appointed']), 'supports should return false, cause of missing txid');
+        static::assertFalse($handler->supports($salesChannelMock, ['txaction' => 'paid']), 'supports should return false, cause of missing txid');
         static::assertFalse($handler->supports($salesChannelMock, ['txid' => 1]), 'supports should return false, cause of missing status');
         static::assertFalse($handler->supports($salesChannelMock, []), 'supports should return false, cause of missing data');
         static::assertFalse($handler->supports($salesChannelMock, []), 'supports should return false, cause of missing data');
@@ -38,15 +42,39 @@ class AutoCaptureHandlerTest extends TestCase
         static::assertFalse($handler->supports($salesChannelMock, ['txid' => 1, 'txaction' => TransactionStatusService::ACTION_FAILED]), 'supports should return false, cause of wrong status');
     }
 
-    public function testIsCaptureGotExecuted(): void
+    public function testIsCaptureGotExecutedForPrepayment(): void
     {
+        $orderTransaction = $this->createMock(OrderTransactionEntity::class);
+        $orderTransaction->method('getPaymentMethodId')->willReturn(PayonePrepayment::UUID);
+        $paymentTransaction = $this->createMock(PaymentTransaction::class);
+        $paymentTransaction->method('getOrderTransaction')->willReturn($orderTransaction);
         $dataHandler = $this->createMock(TransactionDataHandlerInterface::class);
-        $dataHandler->method('getPaymentTransactionByPayoneTransactionId')->willReturn($this->createMock(PaymentTransaction::class));
+        $dataHandler->method('getPaymentTransactionByPayoneTransactionId')->willReturn($paymentTransaction);
         $captureService = $this->createMock(AutomaticCaptureServiceInterface::class);
         $captureService->expects(static::once())->method('captureIfPossible');
 
         $request = new Request();
         $request->request->set('txid', 123);
+        $request->request->set('txaction', TransactionStatusService::ACTION_PAID);
+
+        (new AutoCaptureHandler($dataHandler, $captureService))
+            ->process($this->createMock(SalesChannelContext::class), $request);
+    }
+
+    public function testIsCaptureGotExecutedForOpenInvoice(): void
+    {
+        $orderTransaction = $this->createMock(OrderTransactionEntity::class);
+        $orderTransaction->method('getPaymentMethodId')->willReturn(PayoneOpenInvoice::UUID);
+        $paymentTransaction = $this->createMock(PaymentTransaction::class);
+        $paymentTransaction->method('getOrderTransaction')->willReturn($orderTransaction);
+        $dataHandler = $this->createMock(TransactionDataHandlerInterface::class);
+        $dataHandler->method('getPaymentTransactionByPayoneTransactionId')->willReturn($paymentTransaction);
+        $captureService = $this->createMock(AutomaticCaptureServiceInterface::class);
+        $captureService->expects(static::once())->method('captureIfPossible');
+
+        $request = new Request();
+        $request->request->set('txid', 123);
+        $request->request->set('txaction', TransactionStatusService::ACTION_APPOINTED);
 
         (new AutoCaptureHandler($dataHandler, $captureService))
             ->process($this->createMock(SalesChannelContext::class), $request);
@@ -61,6 +89,44 @@ class AutoCaptureHandlerTest extends TestCase
 
         $request = new Request();
         $request->request->set('txid', 123);
+
+        (new AutoCaptureHandler($dataHandler, $captureService))
+            ->process($this->createMock(SalesChannelContext::class), $request);
+    }
+
+    public function testIsCaptureGotNotExecutedForPrepaymentIfActionIsAppointed(): void
+    {
+        $orderTransaction = $this->createMock(OrderTransactionEntity::class);
+        $orderTransaction->method('getPaymentMethodId')->willReturn(PayonePrepayment::UUID);
+        $paymentTransaction = $this->createMock(PaymentTransaction::class);
+        $paymentTransaction->method('getOrderTransaction')->willReturn($orderTransaction);
+        $dataHandler = $this->createMock(TransactionDataHandlerInterface::class);
+        $dataHandler->method('getPaymentTransactionByPayoneTransactionId')->willReturn($paymentTransaction);
+        $captureService = $this->createMock(AutomaticCaptureServiceInterface::class);
+        $captureService->expects(static::never())->method('captureIfPossible');
+
+        $request = new Request();
+        $request->request->set('txid', 123);
+        $request->request->set('txaction', TransactionStatusService::ACTION_APPOINTED);
+
+        (new AutoCaptureHandler($dataHandler, $captureService))
+            ->process($this->createMock(SalesChannelContext::class), $request);
+    }
+
+    public function testIsCaptureGotNotExecutedForOpenInvoiceIfActionIsPaid(): void
+    {
+        $orderTransaction = $this->createMock(OrderTransactionEntity::class);
+        $orderTransaction->method('getPaymentMethodId')->willReturn(PayoneOpenInvoice::UUID);
+        $paymentTransaction = $this->createMock(PaymentTransaction::class);
+        $paymentTransaction->method('getOrderTransaction')->willReturn($orderTransaction);
+        $dataHandler = $this->createMock(TransactionDataHandlerInterface::class);
+        $dataHandler->method('getPaymentTransactionByPayoneTransactionId')->willReturn($paymentTransaction);
+        $captureService = $this->createMock(AutomaticCaptureServiceInterface::class);
+        $captureService->expects(static::never())->method('captureIfPossible');
+
+        $request = new Request();
+        $request->request->set('txid', 123);
+        $request->request->set('txaction', TransactionStatusService::ACTION_PAID);
 
         (new AutoCaptureHandler($dataHandler, $captureService))
             ->process($this->createMock(SalesChannelContext::class), $request);
