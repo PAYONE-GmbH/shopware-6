2afec9409516cb2976d8d622c36310749ba6c37a
Sascha Brüggenolte <sb@netinventors.de>
2025-10-08 14:22:19 +0200

    Commit compiled sources
    Refreshes the compiled storefront and administration sources
    with the latest code changes. This ensures the deployed application
    uses the most current versions of the assets.

---
+++ src/Resources/public/administration/assets/index-BTrSYjkY.js
@@ -0,0 +1,2 @@
+const e=`{% block payone_payment_plugin_icon %} <img class="payone-payment-plugin-icon" :src="assetFilter('payonepayment/plugin.png')" /> {% endblock %}`,t={template:e,computed:{assetFilter(){return Shopware.Filter.getByName("asset")}}};export{t as default};
+//# sourceMappingURL=index-BTrSYjkY.js.map
