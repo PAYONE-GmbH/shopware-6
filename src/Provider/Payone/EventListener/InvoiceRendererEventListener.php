a233a8260cece6518ad091b915ee95f9d581d621
Sascha Brüggenolte <sb@netinventors.de>
2026-01-30 17:50:07 +0100

    Consider only the last transaction

--- src/Provider/Payone/EventListener/InvoiceRendererEventListener.php
+++ src/Provider/Payone/EventListener/InvoiceRendererEventListener.php
@@ -6,10 +6,10 @@ namespace PayonePayment\Provider\Payone\EventListener;
 
 use PayonePayment\DataAbstractionLayer\Aggregate\PayonePaymentOrderTransactionDataEntity;
 use PayonePayment\DataAbstractionLayer\Extension\PayonePaymentOrderTransactionExtension;
-use PayonePayment\Provider\Payone\Dto\SecuredInvoice\InvoiceDocumentDataDto;
 use PayonePayment\Provider\Payone\Extension\SecuredInvoice\SecuredInvoiceDocumentDataExtension;
 use PayonePayment\Provider\Payone\PaymentMethod\SecuredInvoicePaymentMethod;
 use Shopware\Core\Checkout\Document\Event\InvoiceOrdersEvent;
+use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionEntity;
 use Shopware\Core\Checkout\Order\OrderEntity;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -28,53 +28,42 @@ readonly class InvoiceRendererEventListener implements EventSubscriberInterface
         $orders = $event->getOrders();
 
         foreach ($orders as $order) {
-            if ($this->hasSecuredInvoiceTransaction($order)) {
-                $this->addInvoiceDocumentExtension($order);
+            $transaction = $order->getTransactions()?->last();
+            if (
+                $transaction instanceof OrderTransactionEntity
+                && SecuredInvoicePaymentMethod::UUID === $transaction->getPaymentMethodId()
+            ) {
+                $this->addInvoiceDocumentExtension($order, $transaction);
             }
         }
     }
 
-    private function hasSecuredInvoiceTransaction(OrderEntity $order): bool
+    private function addInvoiceDocumentExtension(OrderEntity $order, OrderTransactionEntity $transaction): void
     {
-        if ($order->getTransactions()) {
-            foreach ($order->getTransactions() as $transaction) {
-                if (SecuredInvoicePaymentMethod::UUID === $transaction->getPaymentMethodId()) {
-                    return true;
-                }
-            }
+        $extensions = $transaction->getExtensions();
+        $extension  = $extensions[PayonePaymentOrderTransactionExtension::NAME] ?? null;
+        if (!$extension instanceof PayonePaymentOrderTransactionDataEntity) {
+            return;
         }
 
-        return false;
-    }
-
-    private function addInvoiceDocumentExtension(OrderEntity $order): void
-    {
-        $data = new SecuredInvoiceDocumentDataExtension();
-
-        foreach ($order->getTransactions() as $transaction) {
-            $extensions = $transaction->getExtensions();
-            $extension  = $extensions[PayonePaymentOrderTransactionExtension::NAME] ?? null;
-
-            if ($extension instanceof PayonePaymentOrderTransactionDataEntity) {
-                $txData = $extension->getTransactionData() ?? [];
-                foreach ($txData as $txDatum) {
-                    $clearing = $txDatum['response']['clearing'] ?? [];
-                    if ([] === $clearing) {
-                        continue;
-                    }
-
-                    $dto = new InvoiceDocumentDataDto(
-                        $clearing['BankAccount']['BankAccountHolder'] ?? null,
-                        $clearing['BankAccount']['Iban'] ?? null,
-                        $clearing['BankAccount']['Bic'] ?? null,
-                        $clearing['DueDate'] ?? null,
-                        $clearing['Reference'] ?? null,
-                    );
-                    $data->addDocumentData($dto);
-                }
-            }
+        $txData = $extension->getTransactionData() ?? [];
+        if ([] === $txData) {
+            return;
         }
 
-        $order->addExtension(SecuredInvoiceDocumentDataExtension::EXTENSION_NAME, $data);
+        $txDatum  = end($txData);
+        $clearing = $txDatum['response']['clearing'] ?? [];
+        if ([] === $clearing) {
+            return;
+        }
+
+        $ext = new SecuredInvoiceDocumentDataExtension(
+            $clearing['BankAccount']['BankAccountHolder'] ?? null,
+            $clearing['BankAccount']['Iban'] ?? null,
+            $clearing['BankAccount']['Bic'] ?? null,
+            $clearing['DueDate'] ?? null,
+            $clearing['Reference'] ?? null,
+        );
+        $order->addExtension(SecuredInvoiceDocumentDataExtension::EXTENSION_NAME, $ext);
     }
 }
--- src/Provider/Payone/Extension/SecuredInvoice/SecuredInvoiceDocumentDataExtension.php
+++ src/Provider/Payone/Extension/SecuredInvoice/SecuredInvoiceDocumentDataExtension.php
@@ -4,31 +4,18 @@ declare(strict_types=1);
 
 namespace PayonePayment\Provider\Payone\Extension\SecuredInvoice;
 
-use PayonePayment\Provider\Payone\Dto\SecuredInvoice\InvoiceDocumentDataDto;
 use Shopware\Core\Framework\Struct\Struct;
 
 class SecuredInvoiceDocumentDataExtension extends Struct
 {
     final public const EXTENSION_NAME = 'payone_secured_invoice_data';
 
-    private array $documentData = [];
-
-    public function __construct()
-    {
-    }
-
-    public function addDocumentData(InvoiceDocumentDataDto $documentData): void
-    {
-        $this->documentData[] = $documentData;
-    }
-
-    public function getDocumentData(): array
-    {
-        return $this->documentData;
-    }
-
-    public function setDocumentData(array $documentData): void
-    {
-        $this->documentData = $documentData;
+    public function __construct(
+        public string|null $accountHolder,
+        public string|null $iban,
+        public string|null $bic,
+        public string|null $dueDate,
+        public string|null $reference,
+    ) {
     }
 }
